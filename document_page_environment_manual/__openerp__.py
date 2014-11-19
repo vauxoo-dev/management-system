f939409e65ee9c981da00138480b3f2976570657
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-19 10:31:01 +0300

    Making module installable and nothing else to change

--- document_page_environment_manual/__openerp__.py
+++ document_page_environment_manual/__openerp__.py
@@ -4,38 +4,23 @@
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
-#    This program is free software: you can redistribute it and/or modify
-<<<<<<< HEAD
-#    it under the terms of the GNU General Public License as
-=======
 #    it under the terms of the GNU Affero General Public License as
->>>>>>> Moved document_page_environment_manual to root folder for port
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-<<<<<<< HEAD
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-=======
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
->>>>>>> Moved document_page_environment_manual to root folder for port
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     "name": "Document Management - Wiki - Environment Manual",
     "version": "1.0",
-<<<<<<< HEAD
-    "author": "Savoir-faire Linux",
-=======
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
->>>>>>> Moved document_page_environment_manual to root folder for port
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
     "category": "Generic Modules/Others",
@@ -44,11 +29,7 @@
     "depends": ['mgmtsystem_manuals'],
     "data": ['document_page_environment_manual.xml'],
     "demo": [],
-<<<<<<< HEAD
     "installable": True,
-=======
-    'installable': False,
->>>>>>> Moved document_page_environment_manual to root folder for port
     "certificate": ''
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
